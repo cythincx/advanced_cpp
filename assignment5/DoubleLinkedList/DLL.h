57a87af6a3e777cebe63e91d9b41b26c9195952f
olymcircle <168chengyuxing@sina.com>
2013-11-17 00:00:57 +0800

    add 5 1

--- assignment5/DoubleLinkedList/DLL.h
+++ assignment5/DoubleLinkedList/DLL.h
@@ -2,8 +2,22 @@
 #define DLL_H_INCLUDED
 #include<iostream>
 #include "Node.h"
+#include "CYXString.h"
 using namespace std;
 class DLL{
+private:
+    Node * head;
+    Node * tail;
+    Node * nodePtr;
+    int numElements;
+public:
+DLL();
+DLL( const DLL & );
+DLL & operator=( const DLL & );
+~DLL();
+friend ostream & operator<<(ostream & , const DLL &);
+bool insert( const CYXString & );
+bool remove( const CYXString & );
 };
 
 
